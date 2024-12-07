9f1861a000554bb88953732da1da9d69be192ca1
Kesava-1006 <162858215+Kesava-1006@users.noreply.github.com>
2024-12-07 10:24:34 +0530

    Update ClientDemo.java

--- src/main/java/com/klef/jfsd/springboot/ClientDemo.java
+++ src/main/java/com/klef/jfsd/springboot/ClientDemo.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ClientDemo {
     public static void main(String[] args) {
-        // Use ApplicationConfig.class instead of ApplicationConfig.java
+        
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-        // Fetch and print Employee bean
+        
         Employee employee = context.getBean(Employee.class);
         System.out.println(employee);
 
-        // Fetch and print Course bean
         Course course = context.getBean(Course.class);
         System.out.println(course);
     }
